6fd0af8d2e9a0f96a521439a7fc1aec70e5cb8c1
Yudi Yamane <yudiyamane@gmail.com>
2022-03-12 22:21:44 -0300

    app does not use port number when hosted on vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,17 @@ import axios from "axios";
 const SERVER_ADDRESS = import.meta.env.VITE_SERVER_ADDRESS || 'http://localhost'
 const PORT = import.meta.env.VITE_SERVER_PORT || 5000
 
+let baseURL = ''
+
+if (SERVER_ADDRESS.indexOf('localhost') > 0)
+  baseURL = `${SERVER_ADDRESS}:${PORT}`
+else
+  baseURL = `${SERVER_ADDRESS}`
+
+console.log(baseURL);
 
 const api = axios.create({
-  baseURL: `${SERVER_ADDRESS}:${PORT}`
+  baseURL
 })
 
 export default api
