6de30797af7ff10cc91f04ec8da2b101e642de67
Yudi Yamane <yudiyamane@gmail.com>
2022-05-14 04:40:47 -0300

    removed borders for days row and times column

--- src/presentation/components/Schedule/styles.ts
+++ src/presentation/components/Schedule/styles.ts
@@ -18,6 +18,7 @@ export const Timebox = styled.div<TimeboxProps>`
     ? timeboxColors[props.value] 
     : '#FFFFFF'};
   cursor: pointer;
+  border: 1px solid rgba(0, 0, 0, 0.1);
   
   & {
     transition: all 0.2s ease;
@@ -51,41 +52,6 @@ export const Container = styled.div<ContainerProps>`
     -webkit-user-select: none;
     -o-user-select: none;
     border-radius: 4px;
-  }
-
-  > div#top-left {
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-    border-right: 1px solid rgba(0, 0, 0, 0.1);
-  }
-
-  > div.day {
-    position: relative;
-
-    &::after {
-      position: absolute;
-      content: '';
-      background: rgba(0, 0, 0, 0.1);
-      height: 100%;
-      width: 1px;
-      right: -3px;
-    }
-  }
-
-  > div.time {
-    position: relative;
-
-    &::after {
-      position: absolute;
-      content: '';
-      background: rgba(0, 0, 0, 0.1);
-      height: 1px;
-      width: 100%;
-      bottom: -3px;
-    }
-  }
-
-  > div#top-left, > div.time, > div.day {
-    border-radius: 5px;
   }
 
   > div.day, div.time {
