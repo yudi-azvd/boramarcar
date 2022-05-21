ff592981ba6c2bb55e182a08f2080aa4d2a1a338
Yudi Yamane <yudiyamane@gmail.com>
2022-05-21 19:22:59 -0300

    private route in a different way

--- src/main/routes/RequireAuth.tsx
+++ src/main/routes/RequireAuth.tsx
@@ -1,11 +1,10 @@
 import { useAuth } from '@/presentation/hooks/auth'
-import { Navigate, Route as ReactRoute, RouteProps as ReactRouterRouteProps, useLocation } from 'react-router-dom'
 
-interface RequireAuthProps {
-  children: JSX.Element
-}
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
-const RequireAuth = ({ children }: RequireAuthProps) => {
+// https://stackoverflow.com/questions/70724269/react-router-v6-route-composition-is-it-possible-to-render-a-custom-route
+// https://reactrouter.com/docs/en/v6/components/navigate 
+const RequireAuth = () => {
   const location = useLocation()
   const { user } = useAuth()
   const isAthenticated = !!user.name
@@ -14,7 +13,7 @@ const RequireAuth = ({ children }: RequireAuthProps) => {
     return <Navigate to="/" state={{ from: location }} replace />
   }
 
-  return children
+  return <Outlet />
 }
 
 export default RequireAuth
