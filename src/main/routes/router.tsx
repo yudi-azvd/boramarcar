86134d6da02b1153a5f92f77c3eb8d61d502d817
Yudi Yamane <yudiyamane@gmail.com>
2022-05-21 19:06:05 -0300

    implemented private route

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -2,6 +2,8 @@ import { AuthProvider } from '@/presentation/hooks/auth'
 import ChooseName from '@/presentation/pages/ChooseName'
 import Dashboard from '@/presentation/pages/Dashboard'
 import DevNotes from '@/presentation/pages/DevNotes'
+
+import RequireAuth from './RequireAuth'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const Router: React.FC = () => {
@@ -12,8 +14,14 @@ const Router: React.FC = () => {
           {/* pra fazer autenticação de verdade */}
           {/* https://reactrouter.com/docs/en/v6/components/navigate */}
           <Route path='/' element={<ChooseName />} />
-          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/devnotes' element={<DevNotes />} />
+          <Route path='/dashboard'
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
