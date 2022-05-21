e77245387d01bec9b24a41ae32451d07b32fc582
Yudi Yamane <yudiyamane@gmail.com>
2022-05-21 11:18:12 -0300

    changed name in Timebox tests

--- tests/presentation/components/schedule/Timebox.spec.tsx
+++ tests/presentation/components/schedule/Timebox.spec.tsx
@@ -3,37 +3,38 @@ import '@testing-library/jest-dom'
 import 'jest-styled-components'
 
 import { TimeboxItem } from '@/presentation/components/ScheduleTab/styles'
+import { Availability } from '@/types'
 
 interface SutProps {
-  value?: 'available' | 'busy'
+  availability?: Availability
 }
 
 interface Sut {
   sut: HTMLDivElement
 }
 
-function makeSut({ value }: SutProps): Sut {
-  const { container } = render(<TimeboxItem id="timebox-id" availability={value} />)
+function makeSut({ availability }: SutProps): Sut {
+  const { container } = render(<TimeboxItem id="timebox-id" availability={availability} />)
   return { sut: container.querySelector('#timebox-id') as HTMLDivElement }
 }
 
 describe('Timebox', () => {
   it('should be grey when its value is undefined', () => {
-    const { sut } = makeSut({ value: undefined })
+    const { sut } = makeSut({ availability: undefined })
 
     expect(sut).toMatchSnapshot()
     expect(sut).toHaveStyle(`background: #FFFFFF`)
   })
 
   it('should be green when its value is available', () => {
-    const { sut } = makeSut({ value: 'available' })
+    const { sut } = makeSut({ availability: 'available' })
     
     expect(sut).toMatchSnapshot()
     expect(sut).toHaveStyle(`background: #18DC86`)
   })
   
   it('should be red when its value is busy', () => {
-    const { sut } = makeSut({ value: 'busy' })
+    const { sut } = makeSut({ availability: 'busy' })
 
     expect(sut).toMatchSnapshot()
     expect(sut).toHaveStyle(`background: #E95F63`)
