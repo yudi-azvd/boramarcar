a4d2e5a3a59d5ac9da4e101dc187c2fdc25230ef
Yudi Yamane <yudiyamane@gmail.com>
2022-05-31 03:03:48 -0300

    populates db in development environment

---
+++ src/data/firebase/db/populate.ts
@@ -0,0 +1,55 @@
+import { Room, User } from '@/domain/types';
+import {
+  getDatabase, ref, update,
+} from 'firebase/database';
+
+export const database = getDatabase()
+
+let usersCount = 0
+const roomsCount = 0
+
+async function devCeateUser(username: string) {
+  usersCount++
+  const userId = `u${usersCount}`
+  const usersRef = ref(database, 'users')
+  update(usersRef, {
+    [userId]: {
+      name: username,
+    },
+  })
+
+  return {
+    id: userId,
+    name: username,
+    schedule: {},
+  } as User
+}
+
+async function devCeateRoom(roomname: string, ownerId: string) {
+  const roomsRef = ref(database, 'rooms/')
+  const roomId = `r${roomsCount}`
+
+  update(roomsRef, {
+    [roomId]: {
+      users: { [ownerId]: true },
+      ownerId,
+      status: 'Ativo',
+      name: roomname,
+    },
+  })
+
+  return {
+    name: roomname,
+    id: roomId,
+  } as Room
+}
+
+async function populateDevelopmentDatabase() {
+  const alvares = await devCeateUser('Alvares')
+  await devCeateRoom('Terra Média', alvares.id)
+}
+
+if (import.meta.env.DEV) {
+  console.log('populate DB');
+  populateDevelopmentDatabase()
+}
